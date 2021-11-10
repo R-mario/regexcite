9dccb23e5d59edbc289f07a3cd33a244cc79c202
Mario Ruiz <marioruizperez00@gmail.com>
2021-11-10 23:08:02 +0100

    Add strsplit1()

---
+++ R/strsplit1.R
@@ -0,0 +1,9 @@
+
+##function that splits a string 'x'
+##using 'split' as separator and transform it to a list
+
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
+
+#use_r("name") open/create new file in R directory
